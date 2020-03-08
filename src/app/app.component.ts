8e26271f9ddf04df94b870f393698c15f7abbca5
Neelaka <rneelaka@gmail.com>
2020-03-08 13:36:20 +0530

    Angular Matereal

---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'material';
+  isChecked = true;
+  colours = [
+    { id: 1, name: 'Red'},
+    { id: 2, name: 'Blue'},
+    { id: 3, name: 'Black'},
+  ];
+  colour = 2;
+  minDate = new Date(2016, 8, 1);
+  maxDate = new Date(2025, 8, 1);
+  categories = [
+    {name: 'Beginner'},
+    {name: 'Intermediate'},
+    {name: 'Advance'}
+  ];
+  progress = 0;
+  timer;
+  isLoading = false;
+
+  onChange($event) {
+    console.log($event);
+  }
+  selectCategory(category) {
+    this.categories
+    .filter(c => c !== category)
+    .forEach(c => c['selected'] = false);
+
+    category.selected = !category.selected;
+  }
+
+  constructor(private dialog: MatDialog) {
+
+  // this.timer = setInterval(() => {
+  //   this.progress++;
+  //   // tslint:disable-next-line: triple-equals
+  //   // tslint:disable-next-line: no-unused-expression
+  //   this.progress == 100 ? clearInterval(this.timer) : null;
+  //   }, 20);
+        // this.isLoading = true;
+        // this.getCourses()
+        // .subscribe(x => this.isLoading = false);
+
+
+  }
+
+//   getCourses() {
+// return Observable.timer(2000);
+//   }
+
+  // tslint:disable-next-line: member-ordering
+  // const numbers = timer(5000);
+  // // tslint:disable-next-line: no-unused-expression
+  // numbers.subscribe(x => console.log(x));
+
+  // openDialog() {
+  //   this.dialog.open(EditCourseComponent)
+  //   .afterClosed()
+  //   .subscribe(result => console.log(result));
+  // }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatDialogModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
